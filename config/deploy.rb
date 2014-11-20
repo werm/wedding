b4969a9a0fc74ce865c44b3feff719c522d91e89
Craig Wermert <cwermert@signetaccel.com>
2014-11-20 10:58:17 -0500

    puma configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,10 +34,24 @@ set :pty, true
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_error.log"
+# set :puma_error_log, "#{shared_path}/log/puma_access.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 16]
+# set :puma_workers, 0
+# set :puma_init_active_record, true
+# set :puma_preload_app, true
+
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
@@ -45,8 +59,10 @@ set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 16]
 set :puma_workers, 0
-set :puma_init_active_record, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
 set :puma_preload_app, true
+set :puma_prune_bundler, false
 
 namespace :deploy do
 
