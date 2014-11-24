d2a4bc3bd1c5cc555bdea7a3f989cb9ceacf195f
Craig Wermert <cwermert@signetaccel.com>
2014-11-24 07:50:00 -0500

    F'in servers, man

--- Capfile
+++ Capfile
@@ -6,10 +6,8 @@ require 'capistrano/deploy'
 
 require 'capistrano/rvm'
 require 'capistrano/bundler'
-require 'capistrano/puma'
 require 'capistrano/rails/assets' # for asset handling add
 require 'capistrano/rails/migrations' # for running migrations
 
-require 'capistrano/puma/nginx'
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,15 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
-working_directory "/home/deployer/apps/wedding/current"
+root = "/home/deployer/apps/wedding/current"
+working_directory root
+pid "#{root}/tmp/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/home/deployer/apps/wedding/current/unicorn.pid"
-
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/deployer/apps/wedding/current/unicorn.log"
-stdout_path "/home/deployer/apps/wedding/current/unicorn.log"
-
-# Unicorn socket
 listen "/tmp/unicorn.wedding.sock"
-listen "/tmp/unicorn.wedding.sock"
-
-# Number of processes
-# worker_processes 4
 worker_processes 2
-
-# Time-out
 timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
